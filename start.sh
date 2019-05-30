d928a8bab98a53e250d4916d9a9b4d0f88637d40
Marius Stanca <me@marius.xyz>
2019-05-30 23:34:44 +0300

    Remove reload

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ if [ $# -eq 0 ]; then
     echo "Please give an argument to continue the script. Eg. local or docker."
 else
     if [ "$STAGE" == "local" ]; then
-        gunicorn --reload -b 0.0.0.0:8000 runs.main:app --timeout 90 --log-level info
+        gunicorn -b 0.0.0.0:8000 runs.main:app --timeout 90 --log-level info
     elif [ "$STAGE" == "docker" ]; then
         gunicorn -b 0.0.0.0:8000 runs.main:app --worker-tmp-dir /dev/shm --timeout 90 --log-level info
     fi
