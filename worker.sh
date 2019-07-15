6da30517b7d9197e8b0667940261a114b609eb47
Marius Stanca <me@marius.xyz>
2019-07-15 23:26:39 +0300

    Deployment with docker

--- worker.sh
+++ worker.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+set -e
+
 echo "Runing worker..."
 # concurrency=4 : number of child processes processing the queue
 # purge : purges all waiting tasks before the daemon is started
 # autoscale=10,3 : enable autoscaling by providing max_concurrency,min_concurrency
 
-celery worker -A runs.tasks --autoscale=10,3 --purge --loglevel=INFO
+STAGE=$1
+
+if [ $# -eq 0 ]; then
+    echo "Please give an argument to continue the script. Eg. local or docker."
+else
+    if [ "$STAGE" == "local" ]; then
+        celery worker -A runs.tasks --autoscale=10,3 --purge --loglevel=INFO
+    elif [ "$STAGE" == "docker" ]; then
+       celery worker -A runs.tasks --purge --loglevel=INFO
+    fi
+fi
